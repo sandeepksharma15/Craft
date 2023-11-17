bceb55ebe6e71d36ba45e88fc00339b0d311bca6
Sandeep Sharma <sandeepksharma@hotmail.com>
2023-11-17 15:10:05 +0530

    fix: Js

--- Craft.MediaQuery/Services/ViewportResizeListener.cs
+++ Craft.MediaQuery/Services/ViewportResizeListener.cs
@@ -1,11 +1,13 @@
-﻿
-using Craft.MediaQuery.Models;
+﻿using Craft.MediaQuery.Models;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.JSInterop;
 
 namespace Craft.MediaQuery.Services;
 
-public class ViewportResizeListener(IJSRuntime jsRuntime, IOptions<ResizeOptions>? options = null)
+public class ViewportResizeListener(IJSRuntime jsRuntime,
+    ILogger<ViewportResizeListener> logger,
+    IOptions<ResizeOptions>? options = null)
         : IViewportResizeListener, IAsyncDisposable
 {
     private readonly ResizeOptions _options = options?.Value ?? new ResizeOptions();
@@ -23,22 +25,32 @@ public class ViewportResizeListener(IJSRuntime jsRuntime, IOptions<ResizeOptions
 
     public async ValueTask<ViewportSize> GetViewportSize()
     {
+        logger.LogDebug("[ViewportResizeListener] GetViewportSize Invoked");
+
         var module = await _moduleTask.Value;
         return await module.InvokeAsync<ViewportSize>("getViewportSize");
     }
 
     public async ValueTask<bool> MatchMedia(string mediaQuery)
     {
+        logger.LogDebug("[ViewportResizeListener] MatchMedia Invoked");
+
         var module = await _moduleTask.Value;
         return await module.InvokeAsync<bool>("matchMedia", mediaQuery);
     }
 
     [JSInvokable]
-    public void RaiseOnResized(ViewportSize browserWindowSize)
-        => _onResized?.Invoke(this, browserWindowSize);
+    public void RaiseOnResized(ViewportSize viewportSize)
+    {
+        logger.LogDebug($"[ViewportResizeListener] RaiseOnResized Invoked with Height: [{viewportSize.Height}] Width: [{viewportSize.Height}]");
+
+        _onResized?.Invoke(this, viewportSize);
+    }
 
     protected virtual void Dispose(bool disposing)
     {
+        logger.LogDebug("[ViewportResizeListener] Dispose Invoked");
+
         if (!_disposed)
         {
             if (disposing)
@@ -50,6 +62,7 @@ public class ViewportResizeListener(IJSRuntime jsRuntime, IOptions<ResizeOptions
 
     public async ValueTask DisposeAsync()
     {
+        logger.LogDebug("[ViewportResizeListener] DisposeAsync Invoked");
 #pragma warning disable RCS1075 // Avoid empty catch clause that catches System.Exception.
         try
         {
@@ -68,6 +81,8 @@ public class ViewportResizeListener(IJSRuntime jsRuntime, IOptions<ResizeOptions
 
     private void Subscribe(EventHandler<ViewportSize>? value)
     {
+        logger.LogDebug($"[ViewportResizeListener] Subscribe Invoked with value: [{value}]");
+
         if (_onResized is null)
             Task.Run(async () => await Start().ConfigureAwait(false));
 
@@ -76,6 +91,8 @@ public class ViewportResizeListener(IJSRuntime jsRuntime, IOptions<ResizeOptions
 
     private void Unsubscribe(EventHandler<ViewportSize>? value)
     {
+        logger.LogDebug($"[ViewportResizeListener] Unsubscribe Invoked with value: [{value}]");
+
         _onResized -= value;
         if (_onResized is null)
             Task.Run(async () => await Cancel().ConfigureAwait(false));
@@ -83,12 +100,16 @@ public class ViewportResizeListener(IJSRuntime jsRuntime, IOptions<ResizeOptions
 
     private async ValueTask<bool> Start()
     {
+        logger.LogDebug("[ViewportResizeListener] Start Invoked");
+
         var module = await _moduleTask.Value;
         return await module.InvokeAsync<bool>("listenForResize", DotNetObjectReference.Create(this), _options);
     }
 
     private async ValueTask Cancel()
     {
+        logger.LogDebug("[ViewportResizeListener] Cancel Invoked");
+
         var module = await _moduleTask.Value;
         await module.InvokeVoidAsync("cancelListener");
     }
---
+++ Tests/Craft.MediaQuery.Tests/Services/ViewportResizeListenerServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using Craft.MediaQuery.Services;
+using Microsoft.JSInterop;
+using Moq;
+using Microsoft.Extensions.Logging;
+
+namespace Craft.MediaQuery.Tests.Services;
+
+public class ViewportResizeListenerServiceTests
+{
+}
