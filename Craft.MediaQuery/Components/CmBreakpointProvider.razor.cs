baaad08b6bb69c584a4bcde70dedd644a5d2962e
Sandeep Sharma <sandeepksharma@hotmail.com>
2023-11-19 13:46:04 +0530

    feat: CmBreakpointProvider

---
+++ Craft.MediaQuery/Components/CmBreakpointProvider.razor.cs
@@ -0,0 +1,40 @@
+using Craft.MediaQuery.Enums;
+using Craft.MediaQuery.Models;
+using Craft.MediaQuery.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+
+namespace Craft.MediaQuery.Components;
+
+public partial class CmBreakpointProvider : ComponentBase, IAsyncDisposable
+{
+    [Inject] private IViewportResizeListener _viewportResizeListener { get; set; }
+    [Inject] private ILogger<CmBreakpointProvider> _logger { get; set; }
+
+    public Breakpoint Breakpoint { get; private set; } = Breakpoint.Always;
+
+    [Parameter] public EventCallback<Breakpoint> OnBreakpointChanged { get; set; }
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+            _viewportResizeListener.OnResized += WindowResized;
+    }
+
+    async void WindowResized(object _, ResizeEventArgs resizeEventArgs)
+    {
+        _logger.LogDebug("Window is resized Resized");
+
+        Breakpoint = resizeEventArgs.Breakpoint;
+
+        await OnBreakpointChanged.InvokeAsync(resizeEventArgs.Breakpoint);
+
+        StateHasChanged();
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Tests/Craft.MediaQuery.Tests/Services/ViewportResizeListenerServiceTests.cs
+++ Tests/Craft.MediaQuery.Tests/Services/ViewportResizeListenerServiceTests.cs
@@ -2,9 +2,25 @@
 using Microsoft.JSInterop;
 using Moq;
 using Microsoft.Extensions.Logging;
+using Craft.MediaQuery.Models;
+using FluentAssertions;
+using Bunit;
+using Microsoft.Extensions.Options;
 
 namespace Craft.MediaQuery.Tests.Services;
 
-public class ViewportResizeListenerServiceTests
+public class ViewportResizeListenerServiceTests : TestContext
 {
+    [Fact]
+    public async Task GetViewportSize_ShouldReturnViewportSize()
+    {
+        // Arrange
+        var moduleInterop = JSInterop.SetupModule("./_content/Craft.MediaQuery/resizeListener.js");
+        moduleInterop.Setup<ViewportSize>("getViewportSize").SetResult(new ViewportSize { Width = 1000, Height = 1000 });
+
+        // Setup Mock IjsRuntime
+        var jsRuntimeMock = new Mock<IJSRuntime>();
+
+        
+    }
 }
