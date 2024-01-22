a1ba982de5d094d60cc375574c4adca50cadf408
Sandeep Sharma <sandeepksharma@hotmail.com>
2024-01-22 13:32:27 +0530

    The most significant changes involve the refactoring of the `CreateMemberExpression<T>(this string propertyName)` method in the `ExpressionExtensions` class and the addition of a validation check in the `CreateMemberExpression(this Type type, string memberName)` method in the same class.
    1. The `CreateMemberExpression<T>(this string propertyName)` method in the `ExpressionExtensions` class has been refactored. The original implementation has been replaced with a call to `typeof(T).CreateMemberExpression(propertyName)`, which simplifies the method by delegating the creation of the member expression to another method.
    
    2. A new line of code has been added to the `CreateMemberExpression(this Type type, string memberName)` method in the `ExpressionExtensions` class. This line checks if the `memberName` argument is null or empty and throws an `ArgumentException` if it is. This change ensures that the `memberName` argument is valid before the method proceeds, improving the robustness of the method.

--- Craft.Extensions/Expressions/ExpressionExtensions.cs
+++ Craft.Extensions/Expressions/ExpressionExtensions.cs
@@ -16,25 +16,7 @@ public static class ExpressionExtensions
     /// Thrown if the property name is null or empty, or if the property is not found on the type.
     /// </exception>
     public static LambdaExpression CreateMemberExpression<T>(this string propertyName)
-    {
-        ArgumentException.ThrowIfNullOrEmpty(propertyName, nameof(propertyName));
-
-        try
-        {
-            // Create a parameter expression for the lambda
-            var parameter = Expression.Parameter(typeof(T), "x");
-
-            // Create a member expression for accessing the specified member
-            MemberExpression memberExpression = Expression.PropertyOrField(parameter, propertyName);
-
-            // Create and return the LambdaExpression
-            return Expression.Lambda(memberExpression, parameter);
-        }
-        catch (NullReferenceException)
-        {
-            throw new ArgumentException($"Property {propertyName} not found on type {nameof(T)}");
-        }
-    }
+        => typeof(T).CreateMemberExpression(propertyName);
 
     /// <summary>
     /// Creates a LambdaExpression for accessing a specified member of the given Type.
@@ -49,6 +31,8 @@ public static class ExpressionExtensions
     /// </exception>
     public static LambdaExpression CreateMemberExpression(this Type type, string memberName)
     {
+        ArgumentException.ThrowIfNullOrEmpty(memberName, nameof(memberName));
+
         // Ensure the specified member exists on the given Type
         _ = type.GetMemberByName(memberName)
             ?? throw new ArgumentException($"Property {memberName} not found on type {type}");
