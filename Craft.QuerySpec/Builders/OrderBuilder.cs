951d45e021a07c8963cb6da055c6bbfdb421fd42
Sandeep Sharma <sandeepksharma@hotmail.com>
2024-02-09 12:36:48 +0530

    . OrderBuilder

---
+++ Craft.QuerySpec/Builders/OrderBuilder.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq.Expressions;
+using Craft.Extensions.Expressions;
+using Craft.QuerySpec.Enums;
+using Craft.QuerySpec.Helpers;
+
+namespace Craft.QuerySpec.Builders;
+
+/// <summary>
+/// Builder class for creating order expressions.
+/// </summary>
+[Serializable]
+public class OrderBuilder<T> where T : class
+{
+    #region Public Properties
+
+    /// <summary>
+    /// List of order expressions.
+    /// </summary>
+    public List<OrderInfo<T>> OrderExpressions { get; }
+
+    #endregion Public Properties
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Constructor to initialize the OrderBuilder.
+    /// </summary>
+    public OrderBuilder() => OrderExpressions = [];
+
+    #endregion Public Constructors
+
+    #region Private Methods
+
+    /// <summary>
+    /// Adjusts the order type based on existing expressions.
+    /// </summary>
+    internal OrderTypeEnum AdjustOrderType(OrderTypeEnum orderType)
+    {
+        if (OrderExpressions.Any(x => x.OrderType is OrderTypeEnum.OrderBy or OrderTypeEnum.OrderByDescending))
+            if (orderType is OrderTypeEnum.OrderBy)
+                orderType = OrderTypeEnum.ThenBy;
+            else if (orderType is OrderTypeEnum.OrderByDescending)
+                orderType = OrderTypeEnum.ThenByDescending;
+        return orderType;
+    }
+
+    #endregion Private Methods
+
+    #region Public Methods
+
+    /// <summary>
+    /// Adds an order expression based on a property expression.
+    /// </summary>
+    public OrderBuilder<T> Add(Expression<Func<T, object>> propExpr, OrderTypeEnum orderType = OrderTypeEnum.OrderBy)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(propExpr));
+
+        OrderExpressions.Add(new OrderInfo<T>(propExpr, AdjustOrderType(orderType)));
+
+        return this;
+    }
+
+    /// <summary>
+    /// Adds an order expression based on a property name.
+    /// </summary>
+    public OrderBuilder<T> Add(string propName, OrderTypeEnum orderType = OrderTypeEnum.OrderBy)
+    {
+        var propExpr = ExpressionBuilder.GetPropertyExpression<T>(propName);
+
+        OrderExpressions.Add(new OrderInfo<T>(propExpr, AdjustOrderType(orderType)));
+
+        return this;
+    }
+
+    /// <summary>
+    /// Clears all order expressions.
+    /// </summary>
+    public OrderBuilder<T> Clear()
+    {
+        OrderExpressions.Clear();
+
+        return this;
+    }
+
+    /// <summary>
+    /// Removes an order expression based on a property expression.
+    /// </summary>
+    public OrderBuilder<T> Remove(Expression<Func<T, object>> propExpr)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(propExpr));
+
+        var comparer = new ExpressionSemanticEqualityComparer();
+
+        var orderInfo = OrderExpressions.Find(x => comparer.Equals(x.OrderItem, propExpr));
+
+        if (orderInfo != null)
+            OrderExpressions.Remove(orderInfo);
+
+        return this;
+    }
+
+    /// <summary>
+    /// Removes an order expression based on a property name.
+    /// </summary>
+    public OrderBuilder<T> Remove(string propName)
+    {
+        Remove(ExpressionBuilder.GetPropertyExpression<T>(propName));
+
+        return this;
+    }
+
+    #endregion Public Methods
+}
---
+++ Tests/Craft.QuerySpec.Tests/Builders/OrderBuilderTests.cs
@@ -0,0 +1,164 @@
+﻿using System.Linq.Expressions;
+using Craft.QuerySpec.Builders;
+using Craft.QuerySpec.Enums;
+using Craft.QuerySpec.Helpers;
+using Craft.TestHelper.Models;
+using FluentAssertions;
+
+namespace Craft.QuerySpec.Tests.Builders;
+
+public class OrderBuilderTests
+{
+    #region Public Methods
+
+    [Fact]
+    public void Add_Method_Should_Add_OrderExpression()
+    {
+        // Arrange
+        var orderBuilder = new OrderBuilder<Company>();
+
+        // Act
+        orderBuilder.Add(x => x.Name);
+
+        // Assert
+        orderBuilder.OrderExpressions.Should().NotBeEmpty();
+        orderBuilder.OrderExpressions[0].OrderItem.Should().NotBeNull();
+        orderBuilder.OrderExpressions[0].OrderItem.Body.ToString().Should().Be("x.Name");
+    }
+
+    [Fact]
+    public void Add_WithExistingOrderBy_ShouldAdjustOrderTypeToThenBy()
+    {
+        // Arrange
+        var orderBuilder = new OrderBuilder<Company>();
+        Expression<Func<Company, object>> propExpr1 = x => x.Id;
+        Expression<Func<Company, object>> propExpr2 = x => x.Name;
+        orderBuilder.Add(propExpr1);
+
+        // Act
+        var result = orderBuilder.Add(propExpr2, OrderTypeEnum.OrderBy);
+
+        // Assert
+        result.OrderExpressions[1].OrderType.Should().Be(OrderTypeEnum.ThenBy);
+    }
+
+    [Fact]
+    public void Add_WithExistingOrderByDescending_ShouldAdjustOrderTypeToThenByDescending()
+    {
+        // Arrange
+        var orderBuilder = new OrderBuilder<Company>();
+        Expression<Func<Company, object>> propExpr1 = x => x.Id;
+        Expression<Func<Company, object>> propExpr2 = x => x.Name;
+        orderBuilder.Add(propExpr1, OrderTypeEnum.OrderByDescending);
+
+        // Act
+        var result = orderBuilder.Add(propExpr2, OrderTypeEnum.OrderBy);
+
+        // Assert
+        result.OrderExpressions[1].OrderType.Should().Be(OrderTypeEnum.ThenBy);
+    }
+
+    [Fact]
+    public void AddProperty_Method_Should_Add_OrderExpression()
+    {
+        // Arrange
+        var orderBuilder = new OrderBuilder<Company>();
+
+        // Act
+        orderBuilder.Add("Name");
+
+        // Assert
+        orderBuilder.OrderExpressions.Should().NotBeEmpty();
+        orderBuilder.OrderExpressions[0].OrderItem.Should().NotBeNull();
+    }
+
+    [Theory]
+    [InlineData(OrderTypeEnum.OrderBy)]
+    [InlineData(OrderTypeEnum.OrderByDescending)]
+    public void AdjustOrderType_WhenExistingOrderExpressionsPresent_ShouldReturnThenBy(OrderTypeEnum existingOrderType)
+    {
+        // Arrange
+        var orderBuilder = new OrderBuilder<object>();
+        orderBuilder.OrderExpressions.Add(new OrderInfo<object>(null, existingOrderType));
+
+        // Act
+        var adjustedOrderType = orderBuilder.AdjustOrderType(OrderTypeEnum.OrderBy);
+
+        // Assert
+        adjustedOrderType.Should().Be(OrderTypeEnum.ThenBy);
+    }
+
+    [Theory]
+    [InlineData(OrderTypeEnum.OrderBy)]
+    [InlineData(OrderTypeEnum.OrderByDescending)]
+    public void AdjustOrderType_WhenExistingOrderExpressionsPresentAndOrderTypeIsDescending_ShouldReturnThenByDescending(OrderTypeEnum existingOrderType)
+    {
+        // Arrange
+        var orderBuilder = new OrderBuilder<object>();
+        orderBuilder.OrderExpressions.Add(new OrderInfo<object>(null, existingOrderType));
+
+        // Act
+        var adjustedOrderType = orderBuilder.AdjustOrderType(OrderTypeEnum.OrderByDescending);
+
+        // Assert
+        adjustedOrderType.Should().Be(OrderTypeEnum.ThenByDescending);
+    }
+
+    [Fact]
+    public void AdjustOrderType_WhenNoExistingOrderExpressions_ShouldReturnOriginalOrderType()
+    {
+        // Arrange
+        var orderBuilder = new OrderBuilder<object>();
+
+        // Act
+        var adjustedOrderType = orderBuilder.AdjustOrderType(OrderTypeEnum.OrderBy);
+
+        // Assert
+        adjustedOrderType.Should().Be(OrderTypeEnum.OrderBy);
+    }
+
+    [Fact]
+    public void Clear_Method_Should_Empty_OrderExpressions()
+    {
+        // Arrange
+        var orderBuilder = new OrderBuilder<Company>();
+        orderBuilder.Add(x => x.Name);
+
+        // Act
+        orderBuilder.Clear();
+
+        // Assert
+        orderBuilder.OrderExpressions.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Remove_Method_Should_Remove_OrderExpression()
+    {
+        // Arrange
+        var orderBuilder = new OrderBuilder<Company>();
+        Expression<Func<Company, object>> orderExpr = x => x.Name;
+        orderBuilder.Add(orderExpr);
+
+        // Act
+        orderBuilder.Remove(orderExpr);
+
+        // Assert
+        orderBuilder.OrderExpressions.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void RemoveProperty_Method_Should_Remove_OrderExpression()
+    {
+        // Arrange
+        var orderBuilder = new OrderBuilder<Company>();
+        orderBuilder.Add("Name");
+
+        // Act
+        orderBuilder.Remove("Name");
+
+        // Assert
+        orderBuilder.OrderExpressions.Should().BeEmpty();
+    }
+
+    #endregion Public Methods
+}
