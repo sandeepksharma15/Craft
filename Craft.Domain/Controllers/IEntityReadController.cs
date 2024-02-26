f2a717c787908778a5af941456ac98e8d7b90f76
Sandeep Sharma <sandeepksharma@hotmail.com>
2024-02-26 14:30:35 +0530

    Add new interfaces and update project file
    Updated the `Craft.Domain.csproj` file to include a new framework reference to `Microsoft.AspNetCore.App`. Added new interfaces `IEntityChangeController` and `IEntityReadController` in their respective files, with methods for managing entities. Updated `IEntityController.cs` to include a new interface `IEntityController` that extends `IEntityChangeController` with additional methods. Each interface has two versions, one with a key type and one with a predefined key type.

---
+++ Craft.Domain/Controllers/IEntityReadController.cs
@@ -0,0 +1,15 @@
+﻿using Craft.Domain.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Craft.Domain.Controllers;
+
+public interface IEntityReadController<T, TKey> : IEntityController where T : class, IEntity<TKey>, new()
+{
+    Task<ActionResult<IAsyncEnumerable<T>>> GetAllAsync(bool includeDetails, CancellationToken cancellationToken = default);
+
+    Task<ActionResult<T>> GetAsync(TKey id, bool includeDetails, CancellationToken cancellationToken = default);
+
+    Task<ActionResult<long>> GetCountAsync(CancellationToken cancellationToken = default);
+}
+
+public interface IEntityReadController<T> : IEntityReadController<T, KeyType> where T : class, IEntity, new();
