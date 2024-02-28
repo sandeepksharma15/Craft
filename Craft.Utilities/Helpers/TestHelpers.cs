b9fc264365d7a5aeee5c558643adc48a1007c857
Sandeep Sharma <sandeepksharma@hotmail.com>
2024-02-28 19:45:35 +0530

    Updated package references, added new test classes
    Updated `Craft.Utilities.csproj` to include new package references and set `IsTestProject` to `false`. Updated `Microsoft.NET.Test.Sdk` package reference version in multiple project files. Added new `TestHelpers` class in `TestHelpers.cs` for comparing object data. Added `BaseMapperTests` class in `BaseMapperTests.cs` for validating object mapping. Added `TestHelpersTest` class in `TestHelpersTest.cs` for validating `TestHelpers` functionality.

---
+++ Craft.Utilities/Helpers/TestHelpers.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using FluentAssertions;
+
+namespace Craft.Utilities.Helpers;
+
+public static class TestHelpers
+{
+    /// <summary>
+    /// Helper Method to Compare If Two Objects Have The Same Set Of Data
+    /// </summary>
+    /// <typeparam name="T">The Base Type Of Two Objects</typeparam>
+    /// <param name="one">One Object Derived From T</param>
+    /// <param name="another">Another Object Derived From T</param>
+    public static void AreTheySame<T>(T one, T another)
+    {
+        ArgumentNullException.ThrowIfNull(one, nameof(one));
+        ArgumentNullException.ThrowIfNull(another, nameof(another));
+
+        foreach (PropertyInfo field in typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance))
+        {
+            object oneValue = field.GetValue(obj: one);
+            object anotherValue = field.GetValue(obj: another);
+
+            Type type = field.PropertyType;
+
+            if ((type != typeof(string) && typeof(IEnumerable<string>).IsAssignableFrom(type)) || (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>)))
+                oneValue.Should().BeEquivalentTo(anotherValue);
+            else
+                oneValue.Should().Be(anotherValue);
+        }
+    }
+
+    /// <summary>
+    /// Helper Method to Compare If Another Object Is Having Same Set Of Data As This One
+    /// </summary>
+    /// <typeparam name="T">The Base Type</typeparam>
+    /// <param name="one">THIS Object Derived From T</param>
+    /// <param name="another">Another Object Derived From T</param>
+    public static void ShouldBeSameAs<T>(this T one, T another)
+        => AreTheySame(one, another);
+}
---
+++ Tests/Craft.Utilities.Tests/Helpers/TestHelpersTest.cs
@@ -0,0 +1,81 @@
+﻿using Craft.Utilities.Helpers;
+using FluentAssertions;
+
+namespace Craft.Utilities.Tests.Helpers;
+
+public class TestHelpersTest
+{
+    [Fact]
+    public void AreTheySame_SimpleObjects_SameValues_NoExceptions()
+    {
+        // Arrange
+        var obj1 = new { Name = "Test", Age = 30 };
+        var obj2 = new { Name = "Test", Age = 30 };
+
+        // Act & Assert
+        TestHelpers.AreTheySame(obj1, obj2);
+    }
+
+    [Fact]
+    public void AreTheySame_SimpleObjects_DifferentValues_ThrowsAssertionError()
+    {
+        // Arrange
+        var obj1 = new { Name = "Test", Age = 30 };
+        var obj2 = new { Name = "Test", Age = 25 }; // Age is different
+
+        // Act & Assert
+        Action act = () => TestHelpers.AreTheySame(obj1, obj2);
+
+        act.Should().ThrowExactly<Xunit.Sdk.XunitException>();
+    }
+
+    [Fact]
+    public void AreTheySame_ObjectsWithCollections_SameValues_NoExceptions()
+    {
+        // Arrange
+        var list1 = new List<string> { "Item1", "Item2" };
+        var obj1 = new { Name = "Test", List = list1 };
+        var obj2 = new { Name = "Test", List = new List<string> { "Item1", "Item2" } }; // Same content list
+
+        // Act & Assert
+        TestHelpers.AreTheySame(obj1, obj2);
+    }
+
+    [Fact]
+    public void AreTheySame_ObjectsWithCollections_DifferentValues_ThrowsAssertionError()
+    {
+        // Arrange
+        var list1 = new List<string> { "Item1", "Item2" };
+        var obj1 = new { Name = "Test", List = list1 };
+        var obj2 = new { Name = "Test", List = new List<string> { "Item1", "Item3" } }; // Different list content
+
+        // Act & Assert
+        Action act = () => TestHelpers.AreTheySame(obj1, obj2);
+
+        act.Should().ThrowExactly<Xunit.Sdk.XunitException>();
+    }
+
+    [Fact]
+    public void ShouldBeSameAs_SimpleObjects_SameValues_NoExceptions()
+    {
+        // Arrange
+        var obj1 = new { Name = "Test", Age = 30 };
+        var obj2 = new { Name = "Test", Age = 30 };
+
+        // Act & Assert
+        obj1.ShouldBeSameAs( obj2);
+    }
+
+    [Fact]
+    public void ShouldBeSameAs_SimpleObjects_DifferentValues_ThrowsAssertionError()
+    {
+        // Arrange
+        var obj1 = new { Name = "Test", Age = 30 };
+        var obj2 = new { Name = "Test", Age = 25 }; // Age is different
+
+        // Act & Assert
+        Action act = () => TestHelpers.AreTheySame(obj1, obj2);
+
+        act.Should().ThrowExactly<Xunit.Sdk.XunitException>();
+    }
+}
