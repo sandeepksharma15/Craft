d0ba3b9f17c16dbd08c5cef418d4c32fa739574d
Sandeep <sandeepksharma15@gmail.com>
2024-11-02 13:23:16 +0530

    Initial database setup and refactor user/role models
    - Updated `Craft.Data.csproj` and `Craft.Infrastructure.csproj` to include new code analysis packages.
    - Changed namespace in `InfrastructureExtensions.cs` to `Craft.Infrastructure`.
    - Added global using directive for `KeyType` as `long` in `Usings.cs`.
    - Introduced `AppDbContext` class inheriting from `IdentityDbContext`.
    - Added initial migration `20241102074947_InitialDb.cs` for database schema setup.
    - Refactored `AppUser` class to inherit from `IdentityUser<TKey>` and implement `ICraftUser<TKey>`.
    - Created `AppRole` class with additional properties and methods.
    - Updated various files to use new `Domain.Security.Models` namespace.
    - Removed old `AppDbContext` and `AppUser` class definitions.

--- Craft.Infrastructure/InfrastructureExtensions.cs
+++ Craft.Infrastructure/InfrastructureExtensions.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 
-namespace AppFrame.Infrastructure;
+namespace Craft.Infrastructure;
 
 public static class InfrastructureExtensions
 {
