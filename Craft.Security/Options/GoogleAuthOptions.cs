b68aade380bfcc1037e4a72998a1deb762e5b50d
Sandeep <sandeepksharma15@gmail.com>
2024-11-02 14:07:32 +0530

    Add new services, configurations, and refactor Program.cs
    - Added `GoogleAuthOptions` class for Google authentication settings with validation.
    - Introduced `MapsterSettings` class for custom mappings using Mapster.
    - Created `Services.csproj` targeting .NET 8.0 with necessary references.
    - Implemented `IdentityUserProvider` to provide the current user via `IHttpContextAccessor`.
    - Added configuration files for different environments (Development, Production, etc.) for logging, authentication, database, Hangfire, and mail settings.
    - Updated `ServiceCollectionExtensions` to include methods for adding various services.
    - Added `AddConfigurations` method in `WebApplicationBuilderExtensions` to configure settings from JSON files based on the environment.
    - Updated `Cualifi.sln` to include the new `Services` project and configured build settings.
    - Refactored `Program.cs` to improve logging, configuration management, and service registration. Initialized `CraftLogger` and added a try-catch block for exception handling during startup.
    - Updated `Web.csproj` to include new package references for Google authentication, MudBlazor, and project references for Craft.Logging, Services, and Craft.Security.

---
+++ Craft.Security/Options/GoogleAuthOptions.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Craft.Security.Options;
+
+public class GoogleAuthOptions : IValidatableObject
+{
+    public const string SectionName = "Authentication:Google";
+
+    public string ClientId { get; set; }
+    public string ProjectId { get; set; } = string.Empty;
+    public string AuthUri { get; set; } = string.Empty;
+    public string TokenUri { get; set; } = string.Empty;
+    public string AuthProviderX509CertUrl { get; set; } = string.Empty;
+    public string ClientSecret { get; set; }
+    public string[] RedirectUris { get; set; } = [];
+    public string[] JavascriptOrigins { get; set; } = [];
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (ClientId.IsNullOrEmpty())
+            yield return new ValidationResult(
+                $"{nameof(GoogleAuthOptions)}.{nameof(ClientId)} is not configured",
+                [nameof(ClientId)]);
+
+        if (ClientSecret.IsNullOrEmpty())
+            yield return new ValidationResult(
+                $"{nameof(GoogleAuthOptions)}.{nameof(ClientSecret)} is not configured",
+                [nameof(ClientSecret)]);
+
+        if (RedirectUris.IsNullOrEmpty())
+            yield return new ValidationResult(
+                $"{nameof(GoogleAuthOptions)}.{nameof(RedirectUris)} is not configured",
+                [nameof(RedirectUris)]);
+    }
+}
