153c91630acb7a100e57f1eb0aec39b6e1a8b364
Sandeep Sharma <sandeepksharma15@gmail.com>
2024-12-05 11:05:58 +0530

    . FileUpload WIP

--- Craft.Utilities/Services/FileUploadService.cs
+++ Craft.Utilities/Services/FileUploadService.cs
@@ -9,27 +9,28 @@ public class FileUploadService
     /// </summary>
     /// <param name="destFilePath">Destination path to save the file</param>
     /// <param name="sourceFile">The source file being uploaded.</param>
-    /// <param name="allowedSizeLimit">Maximum allowed file size in MB</param>
     /// <param name="progressCallback">Callback to report upload progress (0-100)</param>
+    /// <param name="allowedSizeLimit">Maximum allowed file size in MB</param>
     /// <returns>A task representing the asynchronous operation.</returns>
     public static async Task UploadFileAsync(string? destFilePath, IBrowserFile sourceFile, Action<int> progressCallback = null,
         long allowedSizeLimit = 10)
     {
-        ArgumentNullException.ThrowIfNull(sourceFile);
-        ArgumentNullException.ThrowIfNullOrWhiteSpace(destFilePath);
-
-        if (sourceFile.Size > (allowedSizeLimit * 1024 * 1024))
-            throw new InvalidOperationException($"File size exceeds the allowed limit of {allowedSizeLimit} MB.");
-
         const int bufferSize = 16 * 1024; // 16 KB buffer size
-
+    
         byte[] buffer = new byte[bufferSize];
+        long allowedFileSize = allowedSizeLimit * 1024 * 1024;
         long totalBytesRead = 0;
         long totalFileSize = sourceFile.Size;
 
+        ArgumentNullException.ThrowIfNull(sourceFile);
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(destFilePath);
+
+        if (sourceFile.Size > allowedFileSize)
+            throw new InvalidOperationException($"File size exceeds the allowed limit of {allowedSizeLimit} MB.");
+
         try
         {
-            using (var sourceStream = sourceFile.OpenReadStream())
+            using (var sourceStream = sourceFile.OpenReadStream( maxAllowedSize: allowedFileSize))
             using (var destinationStream = new FileStream(destFilePath, FileMode.Create, FileAccess.Write, FileShare.None, bufferSize, useAsync: true))
             {
                 int bytesRead;
