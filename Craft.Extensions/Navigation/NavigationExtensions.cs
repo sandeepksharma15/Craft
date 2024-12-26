b3e32a943d29dc7c6911df4aff2a9ea63dcf9769
Sandeep Sharma <sandeepksharma15@gmail.com>
2024-12-26 17:12:14 +0530

    Update project dependencies and enhance navigation
    - Added new package references for ASP.NET Core and EF Core.
    - Introduced `NavigationExtensions` and `NavigationHelper` for improved URI management.
    - Added migration to update `ConcurrencyStamp` values in `AP_Products` and `AP_Steps`.
    - Enhanced UI with new CSS styles in `AiScanningReasons.razor.css`.
    - Updated dialog titles and content in `AiScanComplete.razor`.
    - Modified `AiScanning.razor` to include return URL for credit purchases.
    - Renamed enum value in `StepType.cs` for consistency.
    - Improved user experience in `AiScanning.razor` with updated button icons.
    - Highlighted key phrases in `AiScanningReasons.razor` for better readability.

---
+++ Craft.Extensions/Navigation/NavigationExtensions.cs
@@ -0,0 +1,19 @@
+﻿#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace Microsoft.AspNetCore.Components;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+public static class NavigationExtensions
+{
+    public static string GetAbsoluteUriWithoutQueryParameters(this NavigationManager navigationManager)
+    {
+        var uri = navigationManager.ToAbsoluteUri(navigationManager.Uri);
+        return uri.GetLeftPart(UriPartial.Path);
+    }
+
+    public static string GetBaseRelativePathWithoutQueryParameters(this NavigationManager navigationManager)
+    {
+        var uri = navigationManager.ToBaseRelativePath(navigationManager.Uri);
+
+        return uri.Split('?')[0];
+    }
+}
