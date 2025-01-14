daceab0847eb37a24a80d604f774b87bcb679142
Sandeep Sharma <sandeepksharma15@gmail.com>
2025-01-14 18:38:27 +0530

    Enhance cover letter handling and update dependencies
    - Updated `Craft.Utilities.csproj` to include new package references and upgraded existing ones.
    - Added `TextConverters` class for converting Markdown to RTF using the `Markdig` library.
    - Introduced `CoverLetterResponse` class for deserializing cover letters from JSON.
    - Modified `SaveCoverLetterAsync` in `JDService.cs` to save cover letters as JSON files.
    - Updated `FileController.cs` to change download file name to `.doc` and utilize new methods for RTF conversion.

---
+++ Craft.Utilities/Helpers/TextConverters.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http;
+using System.Text;
+using Markdig;
+
+namespace Craft.Utilities.Helpers;
+
+public static class TextConverters
+{
+    public static string? ConvertMarkdownToRtf(string? markdown)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(markdown, nameof(markdown));
+
+        string html = Markdown.ToHtml(markdown);
+
+        return ConvertHtmlToRtf(html);
+    }
+
+    private static string ConvertHtmlToRtf(string html)
+    {
+        // Simple HTML-to-RTF conversion (basic)
+        string rtf = html
+            .Replace("<b>", @"\b ")  // Bold start
+            .Replace("</b>", @"\b0 ")  // Bold end
+            .Replace("<strong>", @"\b ")  // Bold start
+            .Replace("</strong>", @"\b0 ")  // Bold end
+            .Replace("<i>", @"\i ")  // Italics start
+            .Replace("</i>", @"\i0 ")  // Italics end
+            .Replace("<br>", @"\line ")  // Line break
+            .Replace("<br />", @"\line ")  // Line break
+            .Replace("<p>", @"\par ")  // Paragraph start
+            .Replace("</p>", @"\par ")  // Paragraph end
+            .Replace("<h1>", @"\b\fs28 ")  // Header 1 start
+            .Replace("</h1>", @"\b0\fs20\par ")  // Header 1 end
+            .Replace("&nbsp;", " ");  // Non-breaking space
+
+        // Wrap in RTF structure
+        return $@"{{\rtf1\ansi\deff0 {rtf}}}";
+    }
+}
