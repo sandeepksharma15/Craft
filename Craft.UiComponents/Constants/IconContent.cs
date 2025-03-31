2d20bfb8c08d050b2096ff5ef401571f3b5cd045
Sandeep Sharma <sandeepksharma15@gmail.com>
2025-03-31 08:41:10 +0530

    Update project versions and refactor icon usage
    Incremented `FileVersion` and `AssemblyVersion` across multiple project files to reflect new versions. Added new icon constants in `IconContent.cs` and updated `BaseLayout.razor` to use these constants for improved maintainability. Updated `_Imports.razor` with new namespaces and made minor formatting adjustments for cleaner code. These changes prepare the projects for deployment and further testing.

---
+++ Craft.UiComponents/Constants/IconContent.cs
@@ -0,0 +1,13 @@
+﻿namespace Craft.UiComponents.Constants;
+
+public static class IconContent
+{
+    public const string Home = "fa-solid fa-house-chimney";
+    public const string GoTop = "fa-solid fa-chevron-up";
+
+    public const string HeartBorder = "fa-regular fa-heart";
+    public const string HeartFilled = "fa-solid fa-heart";
+
+    public const string Save = "fa-solid fa-floppy-disk";
+    public const string Cancel = "fa-solid fa-xmark";
+}
